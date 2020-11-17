ea138582a94da95088552f8f2fe8c76a38132e41
adkingston <adkingston@gmail.com>
2020-11-17 18:06:32 -0500

    initial commit

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/alexander/ivr_assignment/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/alexander/ivr_assignment/build'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/alexander/ivr_assignment/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/alexander/ivr_assignment/src:$ROS_PACKAGE_PATH"
